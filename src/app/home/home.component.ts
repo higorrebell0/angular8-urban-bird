22ef535eaca9459bb216bec8abc9b9cc65f3c1bf
higorrebell0 <higorrebello@gmail.com>
2019-12-11 15:33:35 -0300

    Uploading project

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { OffersService } from '../offers.service';
+import { Offer } from '../shared/offer.model';
+
+// metadata sent to component class through @Component
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  providers: [ OffersService ]
+  // setting the service as provider to the component father and childs
+})
+export class HomeComponent implements OnInit {
+
+  public offers: Array<Offer>;
+  public offersPromiseSync: Array<Offer>;
+
+  constructor(private offersService: OffersService) { }
+  ngOnInit() {
+    this.offersService.getOffers()
+      .then( (data: Array<Offer>) => {
+        this.offers = data;
+      })
+      .catch((param: any) => {
+        console.log(param);
+      });
+  }
+}
---
+++ src/app/offers.service.ts
@@ -0,0 +1,50 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Offer } from './shared/offer.model';
+
+@Injectable()
+export class OffersService {
+
+  constructor(private http: HttpClient) { }
+
+  public getOffers(): Promise<Array<Offer>> {
+    // make a http request and return a promise Array<Offer>
+    return this.http.get('http://localhost:3000/ofertas')
+      .toPromise()
+      .then((response: any) => response );
+  }
+
+  // ---------------------------------------------------------------------------
+
+  // sync version
+  public getOffersPromiseSync(): Promise<Array<Offer>> {
+    return new Promise((resolve, reject) => { // expects a callback function, an action
+      // some proccess that call the resolve or the reject
+      resolve(this.offers);
+    });
+  }
+
+  // resolve and reject - sync
+  public getOffersPromiseSync2(): Promise<Array<Offer>> {
+    return new Promise((resolve, reject) => {
+      const deuCerto = false;
+      if (deuCerto) {
+        resolve(this.offers);
+      } else {
+        reject({ codigo_erro: 404, mensagem_erro: 'Servidor não encontrado.' });
+      }
+    });
+  }
+
+  // simulating async
+  public getOffersPromiseAsync(): Promise<Array<Offer>> {
+    return new Promise((resolve, reject) => {
+      const deuCerto = true;
+      if (deuCerto) {
+        setTimeout(() => { resolve(this.offers); }, 3000);
+      } else {
+        reject({ codigo_erro: 404, mensagem_erro: 'Servidor não encontrado.' });
+      }
+    });
+  }
+}
