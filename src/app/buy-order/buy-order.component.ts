141e9a63cbf71fe9c44dd6aa4e7200774b927789
Higor <higorrebello@gmail.com>
2020-01-14 16:35:25 -0300

    Validation buy order form

--- src/app/buy-order/buy-order.component.ts
+++ src/app/buy-order/buy-order.component.ts
@@ -7,9 +7,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuyOrderComponent implements OnInit {
 
+  public address = '';
+  public num = '';
+  public complement = '';
+  public paymentType = '';
+
+  // validation
+  public validAddress = false;
+  public validNumber = false;
+  public validComplement = false;
+  public validPayment = false;
+
+  // inicial state (pristine)
+  public pristineAddress = true;
+  public pristineNumber = true;
+  public pristineComplement = true;
+  public pristinePayment = true;
+
+  // button control
+  public stateForm = 'disabled';
   constructor() { }
 
   ngOnInit() {
   }
 
+  public updateAddress(address: string): void {
+    this.address = address;
+    this.pristineAddress = false;
+    if (this.address.length > 3) {
+      this.validAddress = true;
+    } else {
+      this.validAddress = false;
+    }
+
+    this.checkForm();
+  }
+
+  public updateNumber(num: string): void {
+    this.num = num;
+    this.pristineNumber = false;
+    if (this.num.length > 0) {
+      this.validNumber = true;
+    } else {
+      this.validNumber = false;
+    }
+
+    this.checkForm();
+  }
+
+  public updateComplement(complement: string): void {
+    this.complement = complement;
+
+    this.checkForm();
+  }
+
+  public updatePayment(paymentType: string): void {
+    this.paymentType = paymentType;
+    this.pristinePayment = false;
+    if (this.paymentType.length > 0) {
+      this.validPayment = true;
+    } else {
+      this.validPayment = false;
+    }
+
+    this.checkForm();
+  }
+
+  public checkForm(): void {
+    if (this.validAddress === true
+      && this.validNumber === true
+      && this.validPayment === true) {
+        this.stateForm = '';
+      } else {
+        this.stateForm = 'disabled';
+      }
+  }
 }
